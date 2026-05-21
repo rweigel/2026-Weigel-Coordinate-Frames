0b8384676716025cdb3987e184c162bfb97e8595
Bob Weigel <rweigel@gmu.edu>
2026-05-21 11:41:22 -0400

    new fig dir

---
+++ cpfigs.sh
@@ -0,0 +1,13 @@
+DATADIR=../2026-Weigel-Coordinate-Frames-Data
+cp $DATADIR/positions/figures/Geotail_SSCWeb-GEI_vs_CDAWeb-GCI.pdf figures/positions
+cp $DATADIR/positions/figures/Geotail_SSCWeb-J2K_vs_CDAWeb-GCI.pdf figures/positions
+cp $DATADIR/positions/figures/Geotail_SSCWeb-GSE_vs_CDAWeb-GSE.pdf figures/positions
+cp $DATADIR/positions/figures/Geotail_SSCWeb-GSM_vs_CDAWeb-GSM.pdf figures/positions
+cp $DATADIR/positions/figures/MMS-2_SSCWeb-GSE_vs_CDAWeb-GSE.pdf figures/positions
+cp $DATADIR/positions/figures/MMS-2_SSCWeb-GSM_vs_CDAWeb-GSM.pdf figures/positions
+cp $DATADIR/positions/figures/MMS-2_SSCWeb-GSE_vs_JPL-GSE.pdf figures/positions
+cp $DATADIR/positions/figures/MMS-2_SSCWeb-GSM_vs_JPL-GSM.pdf figures/positions
+cp $DATADIR/angles/figures/z-delta=1days_20100101-20150101/GEO_GSE.pdf figures/angles/z-delta=1days_20100101-20150101
+cp $DATADIR/angles/figures/z-delta=1days_20100101-20150101/GEO_GSM.pdf figures/angles/z-delta=1days_20100101-20150101
+cp $DATADIR/angles/figures/z-delta=1days_20100101-20150101/GSE_GSM.pdf figures/angles/z-delta=1days_20100101-20150101
+cp $DATADIR/angles/figures/z-delta=1days_20100101-20150101/MAG_GSM.pdf figures/angles/z-delta=1days_20100101-20150101
